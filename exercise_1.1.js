a44127cd2ccb87e1ccb85977b2883997fea89a46
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-04 10:52:55 +0200

    err handling

--- exercise_1.1.js
+++ exercise_1.1.js
@@ -6,6 +6,11 @@ const pokemonArr = data.split(' ');
 //console.log(pokemonArr);
 console.log("GOTTA CATCH 'EM ALL!!!");
 for (let i = 0; i < pokemonArr.length; i++) {
-    console.log(pokemon.getName(pokemonArr[i], 'en'));
+    if (pokemonArr[i] < 150){
+        console.log(pokemon.getName(pokemonArr[i], 'en'));
+    } else {
+        console.log('NOT FOUND')
+    }
+    
 }
 console.log(__dirname);
