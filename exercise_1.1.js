124862d3a77a6435d6ec91534c509127cf0ad44f
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-04 10:26:10 +0200

    read ids from txt, create arr & log pokemon names

--- exercise_1.1.js
+++ exercise_1.1.js
@@ -1,2 +1,10 @@
 var fs = require('fs');
 const pokemon = require('pokemon');
+
+const data = fs.readFileSync('./ids_pokemon.txt', 'utf8');
+var pokemonArr = data.split(' ');
+//console.log(pokemonArr);
+
+for (let i = 0; i < pokemonArr.length; i++) {
+    console.log(pokemon.getName(pokemonArr[i], 'en'));
+}
