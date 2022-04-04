6e2887adab330d44f10a35905134b58dfba9e596
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-04 12:49:50 +0200

    text modified, wild pokemon

--- exercise_1.3.js
+++ exercise_1.3.js
@@ -2,14 +2,19 @@ var fs = require('fs');
 const pokemon = require('pokemon');
 
 fs.readFile('./ids_pokemon.txt', 'utf8', function (err, data) {
+    
     const pokemonArr = data.split(' ');
+
     for (let i = 0; i < pokemonArr.length; i++) {
+
         pokemonArr[i] = pokemon.getName(pokemonArr[i], 'en');
+
         fs.appendFile('./names_pokemon.txt', pokemonArr[i] + "\n", 'utf8', function (err) {
             if (err) {
                 return console.log(err);
             }
-            console.log(`You catched a ${pokemonArr[i]}!`);
+            console.log(`You catched a wild ${pokemonArr[i]}!`);
         });
     }
 });
+
