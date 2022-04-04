0732e6c46fd4806e80107737a1c3e95821983421
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-04 10:33:56 +0200

    small adj

--- exercise_1.1.js
+++ exercise_1.1.js
@@ -4,7 +4,7 @@ const pokemon = require('pokemon');
 const data = fs.readFileSync('./ids_pokemon.txt', 'utf8');
 var pokemonArr = data.split(' ');
 //console.log(pokemonArr);
-console.log("GOTTA CATCH 'EM ALL!");
+console.log("GOTTA CATCH 'EM ALL!!!");
 for (let i = 0; i < pokemonArr.length; i++) {
     console.log(pokemon.getName(pokemonArr[i], 'en'));
 }
